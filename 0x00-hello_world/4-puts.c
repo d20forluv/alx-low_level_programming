2090f948612235a48ac7a7ff119f684c35624445
d20forluv <adeutiadedayo@gmail.com>
2023-02-18 12:48:47 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
  *Return: Always 0 (success)
-*/
+ */
 int main(void)
 {
-put ("\"programming is like building a multilingual puzzle");
+puts("\"programming like building a multilingual puzzle");
 return (0)
 }
+
