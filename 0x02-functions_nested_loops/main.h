abdbe54136599a25f8931ac696bc14f3af7a7f28
d20forluv <adeutiadedayo@gmail.com>
2023-02-22 19:43:10 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,11 @@
-#include "main.h"
-
-/**
- * main - print putchar
- * Return: always 0
- */
-
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
-
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
