34dc51c17f9f0389564e2a06c0597a85696b42ca
d20forluv <adeutiadedayo@gmail.com>
2023-02-28 00:30:09 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,16 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
- * _puts - function with one argument
- * @str: one char type argument
- *
- * Description: print a string follow by a new line to stdout
- * Return: none
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-	while (*str)
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
