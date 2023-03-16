5997bf364f34f56ee7aed0953a03d0f1f2824715
d20forluv <adeutiadedayo@gmail.com>
2023-03-16 05:25:15 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-oc_checked(unsigned int b);
+//oc_checked(unsigned int b);
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
