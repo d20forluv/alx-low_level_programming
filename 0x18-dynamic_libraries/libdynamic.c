4a99f41f3cc0658eeede0356d562c509236a3bfe
root <d20forluv@gmail.com>
2023-08-23 06:21:31 -0700

    5

--- 0x18-dynamic_libraries/libdynamic.c
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int add(int a, int b)
 {
@@ -27,7 +28,7 @@ int divide(int a, int b)
 	}
 }
 
-greet(void)
+void greet(void)
 {
 	printf("Hello, world!\n");
 }
